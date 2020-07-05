06e714af83ef0d9c2f34799bd7104c61f5d623a6
Ed Timmer <edtimmer@gmail.com>
2020-07-05 14:18:10 -0400

    revise styles

--- src/components/App.js
+++ src/components/App.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import './App.css';
+import { LoremIpsum } from "lorem-ipsum";
 
 import flowerlogo from "../images/flowerlogo.png";
 import beaver from '../images/beaver.jpg';
@@ -10,43 +11,46 @@ import flower from "../images/flower.jpg";
 import fox from "../images/fox.jpg";
 import parrots from "../images/parrots.jpg";
 
+const lorem = new LoremIpsum({
+  sentencesPerParagraph: {
+    max: 8,
+    min: 4,
+  },
+  wordsPerSentence: {
+    max: 16,
+    min: 4,
+  },
+});
+
 const App = () => {
 
   return (
     <div className="App">
       <div className="header">
-        {/* <div className="logo-container">
-          <img className="logo" src={flowerlogo} alt="img-one" />
-        </div> */}
         <div className="header-text">
           Lo<b>rem</b> Ipsum
         </div>
         <button className="button4">Login</button>
       </div>
 
-      <div className="images-container">
+      <div className="row-container">
         <div className="img-container">
           <img className="img-one" src={bluejays} alt="img-one" />
-
         </div>
         <div className="img-container">
           <img className="img-one" src={flower} alt="img-one" />
-
         </div>
         <div className="img-container">
           <img className="img-one" src={fish} alt="img-one" />
-
         </div>
       </div>
 
-      <div className="images-container">
-        <div className="img-container">
-          <img className="img-two" src={beaver} alt="img-two" />
-
-        </div>
-        <div className="img-container">
-          <img className="img-two" src={fox} alt="img-two" />
-  
+      <div className="row-container">
+        <div className="border-box">
+          <div className="img-container">
+            <img className="img-two" src={fox} alt="img-two" />
+          </div>
+          <div className="text-box">{lorem.generateParagraphs(1)}</div>
         </div>
       </div>
     </div>
